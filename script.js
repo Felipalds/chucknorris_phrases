60e6a8e2c3d4854ae8b7fb92423906a77ee59d13
Luiz Felipe Fonseca <luizfelipefonsecarosa@gmail.com>
2021-09-17 11:24:55 -0300

    Improving the prev and next system

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
+let phrases = []
+
 window.onload = function() {
-    showPhrase()
+    showPhrase('random')
 }
 
-function showPhrase(){
-    fetch("https://api.chucknorris.io/jokes/random")
+function showPhrase(param){
+    fetch("https://api.chucknorris.io/jokes/" + param)
     .then(response => response.json())
     .then(body => {
+        console.log(body)
         const chucknorris_joke_p = document.querySelector("#chucknorris_joke_p")
         chucknorris_joke_p.innerText = body.value
+        document.querySelector("#img").src = body.icon_url
+        document.querySelector("#favicon").href = body.icon_url
+
+        if(param == 'random') {
+            phrases.push(body.id)
+            // console.log(phrases)
+        }
     })
+    return phrases
+
 }
 
 function nextPhrase(){
-    showPhrase();
+    showPhrase('random');
+}
+
+function previousPhrase(){
+    let length = phrases.length
+    showPhrase(phrases[length - 2])
+    length = length - 1
+
 }
